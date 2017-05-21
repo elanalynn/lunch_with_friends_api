fcce956540d6439aa3f19a7b3168bad4243048fa
Elana Kopelevich <ekopelevich@gmail.com>
2017-05-21 11:02:23 -0600

    arbitrary change for heroku

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../helpers/auth');
 
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+  res.send('respond with a resource...');
 });
 
 router.get('/:id', auth.isLoggedIn, (req, res, next) => {
