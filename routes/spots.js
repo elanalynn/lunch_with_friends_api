341df21a6ecfe94da4c3654c2fc809f8b600724b
Elana Kopelevich <ekopelevich@gmail.com>
2017-05-22 20:52:27 -0600

    fix places endpoint to include wider radius, types and keyword

--- routes/spots.js
+++ routes/spots.js
@@ -4,9 +4,10 @@ const request = require('request-promise-native');
 
 router.get('/search', (req, res, next) => {
     let location = req.query.location ? req.query.location : '39.718134,-104.900121';
-    let radius = req.query.radius ? req.query.radius : '5';
+    let radius = req.query.radius ? req.query.radius : '5000';
+    let keyword = req.query.keyword ? req.query.keyword : 'vegan';
 
-    const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=${radius}&key=${process.env.PLACES_API_KEY}`;
+    const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=${radius}&types=restaurant&key=${process.env.PLACES_API_KEY}`;
 
     request.get(placesUrl)
     .then(response => {
